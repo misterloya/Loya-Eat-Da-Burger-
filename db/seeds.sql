6061022bf99ad5220ef1a89dffe4bb0e4270d1f5
Issac Loya <misterloya@gmail.com>
2021-01-20 15:53:33 -0600

    push for heroku test

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burger (name) VALUES ('Molly');
 INSERT INTO burger (name) VALUES ('Charlie');
-INSERT INTO burger (name, sleepy) VALUES ('Poppy', true);
-INSERT INTO burger (name, sleepy) VALUES ('Oscar', true);
-INSERT INTO burger (name, sleepy) VALUES ('Smudge', true);
+INSERT INTO burger (name) VALUES ('Poppy');
+INSERT INTO burger (name) VALUES ('Oscar');
+INSERT INTO burger (name) VALUES ('Smudge');
 INSERT INTO burger (name) VALUES ('Daisy');
